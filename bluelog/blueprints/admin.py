7399fa26b8259250f245b8a695c63ca2080b6949
xichagui <xichagui@gmail.com>
2019-09-10 16:54:10 +0800

    posts manager

--- bluelog/blueprints/admin.py
+++ bluelog/blueprints/admin.py
@@ -6,8 +6,11 @@
 # @Software: PyCharm
 
 from bluelog.extensions import db
-from bluelog.forms import SettingForm
-from flask import Blueprint, flash, redirect, render_template, url_for, abort
+from bluelog.forms import PostForm, SettingForm
+from bluelog.models import Category, Post
+from bluelog.utils import redirect_back
+from flask import (Blueprint, abort, current_app, flash, redirect,
+                   render_template, request, url_for)
 from flask_login import current_user, login_required
 
 admin_bp = Blueprint('admin', __name__)
@@ -32,8 +35,63 @@ def settings():
     return render_template('admin/settings.html', form=form)
 
 
-@admin_bp.route('/new_post')
+@admin_bp.route('/post/manager')
+@login_required
+def manager_post():
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['BLUELOG_MANAGE_POST_PER_PAGE'])
+    posts = pagination.items
+    return render_template('admin/manage_post.html',
+                           pagination=pagination,
+                           posts=posts)
+
+
+@admin_bp.route('/new_post', methods=['GET', 'POST'])
+@login_required
 def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data,
+                    body=form.body.data,
+                    category=Category.query.get(form.category.data))
+        db.session.add(post)
+        db.session.commit()
+        flash('Post created.', 'success')
+        return redirect(url_for('blog.show_post', post_id=post.id))
+    return render_template('admin/new_post.html', form=form)
+
+
+@admin_bp.route('/post/<int:post_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_post(post_id):
+    form = PostForm()
+    post = Post.query.get_or_404(post_id)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.body = form.body.data
+        post.category = Category.query.get(form.category.data)
+        db.session.commit()
+        flash('Post updated', 'success')
+        return redirect(url_for('blog.show_post', post_id=post.id))
+    form.title.data = post.title
+    form.body.data = post.body
+    form.category.data = post.category_id
+    return render_template('admin/edit_post.html', form=form)
+
+
+@admin_bp.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post deleted.', 'success')
+    return redirect_back()
+
+
+@admin_bp.route('/category/manager')
+def manager_category():
     abort(404)
 
 
@@ -42,17 +100,6 @@ def new_category():
     abort(404)
 
 
-@admin_bp.route('/manager_post')
-def manager_post():
-    abort(404)
-
-
-@admin_bp.route('/manager_category')
-def manager_category():
-    abort(404)
-
-
-@admin_bp.route('/manager_comment')
+@admin_bp.route('/comment/manager')
 def manager_comment():
     abort(404)
-
